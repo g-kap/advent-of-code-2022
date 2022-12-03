2d94d181b66d9fe3354fcf8c86bda26ca3317e0b
Gabriel Kapiashvili <git@gabrik.kiev.ua>
2022-12-03 12:42:24 +0100

    common code

---
+++ common/common.go
@@ -0,0 +1,20 @@
+package common
+
+import (
+	"bufio"
+	"os"
+)
+
+func ScanFile(path string) (*bufio.Scanner, func()) {
+	f, err := os.Open("path")
+	if err != nil {
+		panic("can not open file")
+	}
+	sc := bufio.NewScanner(f)
+	return sc, func() {
+		err := f.Close()
+		if err != nil {
+			panic(err)
+		}
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/g-kap/advent-of-code-2022
+
+go 1.19
